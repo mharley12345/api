55293f75df82c8f2c5eca37974c7ef50abe9f2cd
Mike Harley <mharley12345@gmail.com>
2019-09-20 12:34:24 -0700

    setup

--- server.js
+++ server.js
@@ -1,102 +1,22 @@
+  
 const express = require("express");
 const bodyParser = require("body-parser");
-const CORS = require("cors");
-
+const cors = require("cors");
+const port = 5000;
 const app = express();
-const token =
-  "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98";
+const data = require("./colors");
+const token = "ahuBHejkJJiMDhmODZhZi0zNWRhLTQ4ZjItOGZhYi1jZWYzOTA07i73Gebhu98";
 
 app.use(bodyParser.json());
-app.use(CORS());
 
-let colors = [
-  {
-    color: "aliceblue",
-    code: {
-      hex: "#f0f8ff"
-    },
-    id: 1
-  },
-  {
-    color: "limegreen",
-    code: {
-      hex: "#99ddbc"
-    },
-    id: 2
-  },
-  {
-    color: "aqua",
-    code: {
-      hex: "#00ffff"
-    },
-    id: 3
-  },
-  {
-    color: "aquamarine",
-    code: {
-      hex: "#7fffd4"
-    },
-    id: 4
-  },
-  {
-    color: "lilac",
-    code: {
-      hex: "#9a99dd"
-    },
-    id: 5
-  },
-  {
-    color: "softpink",
-    code: {
-      hex: "#dd99ba"
-    },
-    id: 6
-  },
-  {
-    color: "bisque",
-    code: {
-      hex: "#dd9a99"
-    },
-    id: 7
-  },
-  {
-    color: "softyellow",
-    code: {
-      hex: "#dcdd99"
-    },
-    id: 8
-  },
-  {
-    color: "blanchedalmond",
-    code: {
-      hex: "#ffebcd"
-    },
-    id: 9
-  },
-  {
-    color: "blue",
-    code: {
-      hex: "#6093ca"
-    },
-    id: 10
-  },
-  {
-    color: "blueviolet",
-    code: {
-      hex: "#8a2be2"
-    },
-    id: 11
-  }
-];
-
-let nextId = 12;
+app.use(cors());
 
 function authenticator(req, res, next) {
   const { authorization } = req.headers;
   if (authorization === token) {
     next();
   } else {
-    res.status(403).json({ error: "User must be logged in to do that." });
+    res.status(403).json({ error: "User be logged in to do that." });
   }
 }
 
@@ -104,11 +24,9 @@ app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
   if (username === "No" && password === "Mike") {
     req.loggedIn = true;
-    setTimeout(() => {
-      res.status(200).json({
-        payload: token
-      });
-    }, 1000);
+    res.status(200).json({
+      payload: token
+    });
   } else {
     res
       .status(403)
@@ -117,47 +35,11 @@ app.post("/api/login", (req, res) => {
 });
 
 app.get("/api/colors", authenticator, (req, res) => {
-  res.send(colors);
+  setTimeout(() => {
+    res.send(data);
+  }, 1000);
 });
 
-app.post("/api/colors", authenticator, (req, res) => {
-  if (req.body.color !== undefined && req.body.code !== undefined) {
-    const newcolor = req.body;
-    newcolor.id = nextId;
-    colors.push(newcolor);
-  }
-  nextId = nextId + 1;
-  res.status(201).json(colors);
-});
-
-app.put("/api/colors/:id", authenticator, (req, res) => {
-  if (!req.params.id)
-    res.status(400).send("Your request is missing the color id");
-  if (req.body.id === undefined || !req.body.color || !req.body.code) {
-    res
-      .status(422)
-      .send("Make sure your request body has all the fields it needs");
-  }
-  colors = colors.map(color => {
-    if (`${color.id}` === req.params.id) {
-      return req.body;
-    }
-    return color;
-  });
-  res.status(200).send(req.body);
-});
-
-app.delete("/api/colors/:id", authenticator, (req, res) => {
-  if (!req.params.id)
-    res.status(400).send("Your request is missing the color id");
-  colors = colors.filter(color => `${color.id}` !== req.params.id);
-  res.status(202).send(req.params.id);
-});
-
-app.get("/", function(req, res) {
-  res.send("App is working 👍");
-});
-
-app.listen(5000, () => {
-  console.log("Server listening on port 5000");
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
